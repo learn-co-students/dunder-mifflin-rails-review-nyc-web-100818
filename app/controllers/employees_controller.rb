00d8652a1ab2ce6f617380bffcb08a959a00903f
tirempbs <tirem@fastmail.com>
2018-11-08 22:20:10 -0500

    done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,52 @@
 class EmployeesController < ApplicationController
+  before_action :find_employee, only: %i[show edit update]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to new_employee_path
+    end
+  end
+
+  def edit
+    @dogs = Dog.all
+  end
+
+  def update
+    @employee.update(employee_params)
+
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to edit_employee_path
+    end
+  end
+
+  private
+
+  def find_employee
+    @employee = Employee.find_by(id: params[:id])
+  end
+
+  def employee_params
+    params.required(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
 end
