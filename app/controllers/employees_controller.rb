23e8278563d6f94d1abc7581f3555b9198c2c788
github username <github email address>
2018-11-08 20:19:37 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
-end
+  before_action :find_employee, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to new_employee_path(@employee)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @employee.update(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to edit_employee_path(@employee)
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
+  def find_employee
+    @employee = Employee.find_by(id: params[:id])
+  end
+end #end of controller
